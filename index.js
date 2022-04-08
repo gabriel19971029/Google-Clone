58b79d7cc3457f2cb67d0902b4284e72069c1356
gabriel19971029 <clementeaquipresente@yahoo.com>
2022-04-08 14:07:42 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+   let gsearch = function() {
+      let url = 'https://www.google.com/search?q=' + document.getElementById('searchme').value;
+      window.open(url, 'google');
+    }; 
+    let glucky = function() {
+      let url = 'https://www.google.com/search?q=' + document.getElementById('searchme').value + '&btnI';
+      window.open(url, 'google');
+    };
+  
+    $(".left").click(gsearch);
+    $(".right").click(glucky);
+  
+    $("#searchme").keypress(function(e) {
+      if (e.which == 13) {
+        gsearch();
+      }
+    });
+  });
